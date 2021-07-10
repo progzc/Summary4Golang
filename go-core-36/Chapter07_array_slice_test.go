caf945c100d13eaf4a7dbccaa2bcafa81d295a10
progzc <991186676@qq.com>
2021-07-10 17:29:55 +0800

    数组与切片

---
+++ go-core-36/Chapter07_array_slice_test.go
@@ -0,0 +1,87 @@
+package go_core_36
+
+import (
+	"fmt"
+	"testing"
+)
+
+// TestArraySlice 切片的使用
+func TestArraySlice(t *testing.T) {
+	s1 := make([]int, 5)
+	fmt.Printf("The length of s1: %d\n", len(s1))
+	fmt.Printf("The capacity of s1: %d\n", cap(s1))
+	fmt.Printf("The value of s1: %d\n", s1)
+	s2 := make([]int, 5, 8)
+	fmt.Printf("The length of s2: %d\n", len(s2))
+	fmt.Printf("The capacity of s2: %d\n", cap(s2))
+	fmt.Printf("The value of s2: %d\n", s2)
+}
+
+// TestArraySlice2 切片的使用2
+func TestArraySlice2(t *testing.T) {
+	s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	s4 := s3[3:6]
+	s5 := [...]int{1, 2, 3, 4, 5, 6, 7, 8}
+	s6 := s4[0:cap(s4)]
+	fmt.Printf("The length of s4: %d\n", len(s4))
+	fmt.Printf("The capacity of s4: %d\n", cap(s4))
+	fmt.Printf("The value of s4: %d\n", s4)
+	fmt.Printf("%T\n", s3)
+	fmt.Printf("%T\n", s5)
+	fmt.Printf("The length of s6: %d\n", len(s6))
+	fmt.Printf("The capacity of s6: %d\n", cap(s6))
+	fmt.Printf("The value of s6: %d\n", s6)
+}
+
+// TestArraySlice3 切片容量的增长
+func TestArraySlice3(t *testing.T) {
+	// 示例1。
+	s6 := make([]int, 0)
+	fmt.Printf("The capacity of s6: %d\n", cap(s6))
+	for i := 1; i <= 5; i++ {
+		s6 = append(s6, i)
+		fmt.Printf("s6(%d): len: %d, cap: %d\n", i, len(s6), cap(s6))
+	}
+	fmt.Println()
+
+	// 示例2。
+	s7 := make([]int, 1024)
+	fmt.Printf("The capacity of s7: %d\n", cap(s7))
+	s7e1 := append(s7, make([]int, 200)...)
+	fmt.Printf("s7e1: len: %d, cap: %d\n", len(s7e1), cap(s7e1))
+	s7e2 := append(s7, make([]int, 400)...)
+	fmt.Printf("s7e2: len: %d, cap: %d\n", len(s7e2), cap(s7e2))
+	s7e3 := append(s7, make([]int, 600)...)
+	fmt.Printf("s7e3: len: %d, cap: %d\n", len(s7e3), cap(s7e3))
+	fmt.Println()
+
+	// 示例3。
+	s8 := make([]int, 10)
+	fmt.Printf("The capacity of s8: %d\n", cap(s8))
+	s8a := append(s8, make([]int, 11)...)
+	fmt.Printf("s8a: len: %d, cap: %d\n", len(s8a), cap(s8a))
+	s8b := append(s8a, make([]int, 23)...)
+	fmt.Printf("s8b: len: %d, cap: %d\n", len(s8b), cap(s8b))
+	s8c := append(s8b, make([]int, 45)...)
+	fmt.Printf("s8c: len: %d, cap: %d\n", len(s8c), cap(s8c))
+}
+
+// TestArraySlice4 切片容量的增长
+func TestArraySlice4(t *testing.T) {
+	// 示例1。
+	a1 := [7]int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Printf("a1: %v (len: %d, cap: %d)\n",
+		a1, len(a1), cap(a1))
+	s9 := a1[1:4]
+	//s9[0] = 1
+	fmt.Printf("s9: %v (len: %d, cap: %d)\n",
+		s9, len(s9), cap(s9))
+	for i := 1; i <= 5; i++ {
+		s9 = append(s9, i)
+		fmt.Printf("s9(%d): %v (len: %d, cap: %d)\n",
+			i, s9, len(s9), cap(s9))
+	}
+	fmt.Printf("a1: %v (len: %d, cap: %d)\n",
+		a1, len(a1), cap(a1))
+	fmt.Println()
+}
