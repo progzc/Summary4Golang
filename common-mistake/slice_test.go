9182859ce3711c28d8de258c6ef71568f90a554c
progzc <991186676@qq.com>
2021-08-22 21:40:19 +0800

    slice init

---
+++ common-mistake/slice_test.go
@@ -0,0 +1,19 @@
+package common_mistake
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestSlice(t *testing.T) {
+	var s1 []string         // good
+	s2 := []string{}        // bad
+	s3 := make([]string, 0) // bad
+	b1, _ := json.Marshal(s1)
+	b2, _ := json.Marshal(s2)
+	b3, _ := json.Marshal(s3)
+	fmt.Println(s1, s1 == nil, string(b1))
+	fmt.Println(s2, s2 == nil, string(b2))
+	fmt.Println(s3, s3 == nil, string(b3))
+}
