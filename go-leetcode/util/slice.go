4e84ecf9fe696ca8038fa608b2133f87f450af12
progzc <991186676@qq.com>
2021-09-26 23:46:19 +0800

    check for equality on slices without order

---
+++ go-leetcode/util/slice.go
@@ -0,0 +1,22 @@
+package util
+
+// link:https://stackoverflow.com/questions/36000487/check-for-equality-on-slices-without-order
+func SameIntSlice(x, y []int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	diff := make(map[int]int, len(x))
+	for _, _x := range x {
+		diff[_x]++
+	}
+	for _, _y := range y {
+		if _, ok := diff[_y]; !ok {
+			return false
+		}
+		diff[_y] -= 1
+		if diff[_y] == 0 {
+			delete(diff, _y)
+		}
+	}
+	return len(diff) == 0
+}
