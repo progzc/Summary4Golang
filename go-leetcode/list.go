83fe09213577bd8b6a53928247dd1bf03b88f1bb
progzc <991186676@qq.com>
2021-09-30 23:13:23 +0800

    自定义ListNode

---
+++ go-leetcode/list.go
@@ -0,0 +1,29 @@
+package go_leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func initListNode(nums []int) *ListNode {
+	var head, tail *ListNode
+	for _, num := range nums {
+		if head == nil {
+			tail = &ListNode{Val: num}
+			head = tail
+		} else {
+			tail.Next = &ListNode{Val: num}
+			tail = tail.Next
+		}
+	}
+	return head
+}
+
+func listNode2Slice(head *ListNode) []int {
+	var nums []int
+	for head != nil {
+		nums = append(nums, head.Val)
+		head = head.Next
+	}
+	return nums
+}
---
+++ go-leetcode/list_test.go
@@ -0,0 +1,29 @@
+package go_leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestListNode(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []int
+	}{
+		{
+			nums: []int{2, 4, 3},
+			want: []int{2, 4, 3},
+		},
+		{
+			nums: []int{},
+			want: nil,
+		},
+	}
+	for _, test := range tests {
+		fact := listNode2Slice(initListNode(test.nums))
+		if !reflect.DeepEqual(fact, test.want) {
+			t.Errorf("nums=%v,want=%v,fact=%v",
+				test.nums, test.want, fact)
+		}
+	}
+}
