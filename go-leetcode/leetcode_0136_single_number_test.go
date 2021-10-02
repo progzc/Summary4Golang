84b5ece02e302399fc40c3c7cbc8903a287fff98
progzc <991186676@qq.com>
2021-10-02 23:00:26 +0800

    只出现一次的数字

--- go-leetcode/leetcode_0136_single_number_test.go
+++ go-leetcode/leetcode_0136_single_number_test.go
@@ -2,6 +2,9 @@ package go_leetcode
 
 import "testing"
 
+// 题目：给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。
+// 找出那个只出现了一次的元素。
+
 func Test_leetcode_0136_singleNumber(t *testing.T) {
 	type params struct {
 		nums []int
