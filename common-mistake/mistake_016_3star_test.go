f115e2008759707f69703afcb0e684a54bc20579
progzc <991186676@qq.com>
2021-10-10 11:55:59 +0800

    string 类型的值是常量，不可更改

--- common-mistake/mistake_016_3star_test.go
+++ common-mistake/mistake_016_3star_test.go
@@ -11,7 +11,8 @@ import (
 //     将 string 转为 []byte 修改后，再转为 string 即可
 func TestMistake_016(t *testing.T) {
 	wrong016()
-	right016()
+	right016_1()
+	right016_2()
 }
 
 func wrong016() {
@@ -20,10 +21,22 @@ func wrong016() {
 	//fmt.Println(x)
 }
 
-func right016() {
+// 下面的示例并不是更新字符串的正确姿势，因为一个 UTF8 编码的字符可能会占多个字节，
+// 比如汉字就需要 3~4 个字节来存储，此时更新其中的一个字节是错误的。
+func right016_1() {
 	x := "text"
 	xBytes := []byte(x)
 	xBytes[0] = 'T' // 注意此时的 T 是 rune 类型
 	x = string(xBytes)
 	fmt.Println(x) // Text
+}
+
+// 更新字串的正确姿势：
+// 将 string 转为 rune slice（此时 1 个 rune 可能占多个 byte），直接更新 rune 中的字符
+func right016_2() {
+	x := "text"
+	xRunes := []rune(x)
+	xRunes[0] = '我'
+	x = string(xRunes)
+	fmt.Println(x) // 我ext
 }
