10427e5d738b2124b0714f94bee09ea71635f763
progzc <991186676@qq.com>
2021-10-10 12:15:23 +0800

    string 与索引操作符

--- common-mistake/mistake_018_test.go
+++ common-mistake/mistake_018_test.go
@@ -7,7 +7,7 @@ import (
 
 // string 与索引操作符
 // (1) 对字符串用索引访问返回的不是字符，而是一个 byte 值。
-// (2) 需要使用 for range 迭代访问字符串中的字符（unicode code point / rune），
+// (2) 如果需要使用 for range 迭代访问字符串中的字符（unicode code point / rune），
 // 标准库中有 "unicode/utf8" 包来做 UTF8 的相关解码编码。
 // 另外 utf8string 也有像 func (s *String) At(i int) rune 等很方便的库函数。
 func TestMistake_018(t *testing.T) {
