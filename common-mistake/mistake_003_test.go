25beb8a655bad06f8f499de72ddab3e45b148b54
progzc <991186676@qq.com>
2021-10-10 09:11:18 +0800

    未使用的 import

--- common-mistake/mistake_003_test.go
+++ common-mistake/mistake_003_test.go
@@ -18,14 +18,14 @@ import (
 // 如果你 import 一个包，但包中的变量、函数、接口和结构体一个都没有用到的话，将编译失败。
 // 可以使用 _ 下划线符号作为别名来忽略导入的包，从而避免编译错误，这只会执行 package 的 init()
 func TestMistake_003(t *testing.T) {
-	wrong()
-	right()
+	wrong003()
+	right003()
 }
 
-func wrong() {
+func wrong003() {
 }
 
-func right() {
+func right003() {
 	_ = log.Println
 	_ = time.Now
 }
