e655a66245da59386aebfc459cfa918984b5127b
progzc <991186676@qq.com>
2021-10-10 09:57:44 +0800

    map 容量

--- common-mistake/mistake_010_test.go
+++ common-mistake/mistake_010_test.go
@@ -6,11 +6,11 @@ import (
 
 // map 容量
 // 在创建 map 类型的变量时可以指定容量，但不能像 slice 一样使用 cap() 来检测分配空间的大小
-func TestMistake_0010(t *testing.T) {
-	wrong0010()
+func TestMistake_010(t *testing.T) {
+	wrong010()
 }
 
-func wrong0010() {
+func wrong010() {
 	//m := make(map[string]int, 99)
 	//println(cap(m)) // error: invalid argument m1 (type map[string]int) for cap
 }
