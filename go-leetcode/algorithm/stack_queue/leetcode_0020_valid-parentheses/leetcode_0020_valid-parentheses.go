102bfdb34fe1223a627acaba6acd7425a390c981
progzc <991186676@qq.com>
2022-02-26 17:00:34 +0800

    0020.有效的括号 (消消乐解法)

--- go-leetcode/algorithm/stack_queue/leetcode_0020_valid-parentheses/leetcode_0020_valid-parentheses.go
+++ go-leetcode/algorithm/stack_queue/leetcode_0020_valid-parentheses/leetcode_0020_valid-parentheses.go
@@ -1,12 +1,15 @@
 package leetcode_0020_valid_parentheses
 
+import "strings"
+
 // 0020.有效的括号
 // https://leetcode-cn.com/problems/valid-parentheses/
 
 // isValid 栈
 // 时间复杂度: O(n)
 // 空间复杂度: O(n)
-func isValid(s string) bool {
+// 思路：经典的栈解法
+func isValid_1(s string) bool {
 	n := len(s)
 	if n%2 == 1 {
 		return false
@@ -28,4 +31,17 @@ func isValid(s string) bool {
 		}
 	}
 	return len(stack) == 0
+}
+
+// isValid_2 替代
+// 时间复杂度: O(n^2)
+// 空间复杂度: O(n)
+// 思路: 消消乐
+func isValid_2(s string) bool {
+	length := 0
+	for length != len(s) {
+		length = len(s)
+		s = strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(s, "()", ""), "{}", ""), "[]", "")
+	}
+	return len(s) == 0
 }
