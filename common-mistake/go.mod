0702c09ab9a299519dabc622548d98a95435a50e
progzc <991186676@qq.com>
2022-02-26 21:50:26 +0800

    go tool vet的使用

--- common-mistake/go.mod
+++ common-mistake/go.mod
@@ -1,3 +1,5 @@
 module github.com/progzc/Summary4Golang/common-mistake
 
 go 1.14
+
+require github.com/pkg/errors v0.9.1
--- common-mistake/mistake_007_3star_test.go
+++ common-mistake/mistake_007_3star_test.go
@@ -19,9 +19,9 @@ func right007() {
 		// 这是 Go 开发者常犯的错，而且不易被发现
 		// 解决办法:
 		// (1) 使用 vet 工具来诊断这种变量覆盖，Go 默认不做覆盖检查，添加 -shadow 选项来启用
-		//	   例如：go tool vet -shadow main.go
+		//	   例如：go tool vet -shadow 6_escape_1.go
 		// (2) vet 不会报告全部被覆盖的变量，可以使用 go-nyet 来做进一步的检测
-		//	   例如：$GOPATH/bin/go-nyet main.go
+		//	   例如：$GOPATH/bin/go-nyet 6_escape_1.go
 		x := 2
 		println(x) // 2
 	}
