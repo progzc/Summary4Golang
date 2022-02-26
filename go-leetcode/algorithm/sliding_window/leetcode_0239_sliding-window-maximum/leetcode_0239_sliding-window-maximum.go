0bc39b0afc5a530ba0be3f8601984c79106940be
progzc <991186676@qq.com>
2022-02-26 20:39:56 +0800

    0239.滑动窗口最大值(单调递减队列)

---
+++ go-leetcode/algorithm/sliding_window/leetcode_0239_sliding-window-maximum/leetcode_0239_sliding-window-maximum.go
@@ -0,0 +1,31 @@
+package leetcode_0239_sliding_window_maximum
+
+// 0239.滑动窗口最大值
+// https://leetcode-cn.com/problems/sliding-window-maximum/
+
+// maxSlidingWindow 单调递减队列
+// 时间复杂度: O(n)
+// 空间复杂度; O(k)
+func maxSlidingWindow(nums []int, k int) []int {
+	q := []int{}
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+	}
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
+	}
+	return ans
+}
