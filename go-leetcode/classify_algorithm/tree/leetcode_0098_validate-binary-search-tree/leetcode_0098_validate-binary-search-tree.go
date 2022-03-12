6842c04f74e6200a608265d513f61b366bb8c692
progzc <991186676@qq.com>
2022-03-12 20:41:05 +0800

    0098.验证二叉搜索树

---
+++ go-leetcode/classify_algorithm/tree/leetcode_0098_validate-binary-search-tree/leetcode_0098_validate-binary-search-tree.go
@@ -0,0 +1,51 @@
+package leetcode_0098_validate_binary_search_tree
+
+import "math"
+
+// 0098.验证二叉搜索树
+// https://leetcode-cn.com/problems/validate-binary-search-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// isValidBST_1 递归 深度优先遍历
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func isValidBST_1(root *TreeNode) bool {
+	var dfs func(root *TreeNode, lower, upper int) bool
+	dfs = func(root *TreeNode, lower, upper int) bool {
+		if root == nil {
+			return true
+		}
+		if root.Val <= lower || root.Val >= upper {
+			return false
+		}
+		return dfs(root.Left, lower, root.Val) && dfs(root.Right, root.Val, upper)
+	}
+	return dfs(root, math.MinInt64, math.MaxInt64)
+}
+
+// isValidBST_2 迭代 广度优先遍历
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func isValidBST_2(root *TreeNode) bool {
+	var stack []*TreeNode
+	pre := math.MinInt64
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= pre {
+			return false
+		}
+		pre = root.Val
+		root = root.Right
+	}
+	return true
+}
