3610178e5c547932028b50628958966dd4b169df
progzc <991186676@qq.com>
2022-03-17 22:39:05 +0800

    0067.二进制求和

---
+++ go-leetcode/classify_algorithm/bit/leetcode_0067_add-binary/leetcode_0067_add-binary.go
@@ -0,0 +1,34 @@
+package leetcode_0067_add_binary
+
+import "strconv"
+
+// 0067.二进制求和
+// https://leetcode-cn.com/problems/add-binary/
+
+func addBinary(a string, b string) string {
+	ans := ""
+	carry := 0
+	lenA, lenB := len(a), len(b)
+	n := max(lenA, lenB)
+	for i := 0; i < n; i++ {
+		if i < lenA {
+			carry += int(a[lenA-i-1] - '0')
+		}
+		if i < lenB {
+			carry += int(b[lenB-i-1] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
---
+++ go-leetcode/classify_algorithm/bit/leetcode_0067_add-binary/leetcode_0067_add-binary_test.go
@@ -0,0 +1,11 @@
+package leetcode_0067_add_binary
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinary(t *testing.T) {
+	str1 := "11"
+	fmt.Printf("%d\n", str1[1]-'0')
+}
