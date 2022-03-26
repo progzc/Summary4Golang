015f92b791ee8f5bfd58da005ab761dd0f11be1a
progzc <991186676@qq.com>
2022-03-26 17:56:00 +0800

    0146.LRU缓存

---
+++ go-leetcode/tool/lru/lru.go
@@ -0,0 +1,85 @@
+package lru
+
+// 0146.LRU缓存
+// https://leetcode-cn.com/problems/lru-cache/
+
+// 时间复杂度: O(1)
+// 空间复杂度: O(n)
+// 思路：双向链表+map+头尾哨兵节点
+
+type LRUCache struct {
+	size     int
+	capacity int
+	cache    map[int]*DLinkedNode
+	head     *DLinkedNode
+	tail     *DLinkedNode
+}
+
+type DLinkedNode struct {
+	key   int
+	value int
+	pre   *DLinkedNode
+	next  *DLinkedNode
+}
+
+func Constructor(capacity int) LRUCache {
+	l := LRUCache{
+		capacity: capacity,
+		cache:    map[int]*DLinkedNode{},
+		head:     &DLinkedNode{key: 0, value: 0},
+		tail:     &DLinkedNode{key: 0, value: 0},
+	}
+	l.head.next = l.tail
+	l.tail.next = l.head
+	return l
+}
+
+func (this *LRUCache) Get(key int) int {
+	if _, ok := this.cache[key]; !ok {
+		return -1
+	}
+	node := this.cache[key]
+	this.moveToHead(node)
+	return node.value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if _, ok := this.cache[key]; !ok {
+		node := &DLinkedNode{key: key, value: value}
+		this.cache[key] = node
+		this.addToHead(node)
+		this.size++
+		if this.size > this.capacity {
+			removed := this.removeTail()
+			delete(this.cache, removed.key)
+			this.size--
+		}
+	} else {
+		node := this.cache[key]
+		node.value = value
+		this.moveToHead(node)
+	}
+}
+
+func (this *LRUCache) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeNode(node *DLinkedNode) {
+	node.pre.next = node.next
+	node.next.pre = node.pre
+}
+
+func (this *LRUCache) addToHead(node *DLinkedNode) {
+	node.pre = this.head
+	node.next = this.head.next
+	this.head.next.pre = node
+	this.head.next = node
+}
+
+func (this *LRUCache) removeTail() *DLinkedNode {
+	node := this.tail.pre
+	this.removeNode(node)
+	return node
+}
