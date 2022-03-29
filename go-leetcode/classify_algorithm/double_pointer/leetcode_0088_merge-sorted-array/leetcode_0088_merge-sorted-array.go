df8892c54b08e5d4e2852d5ebe965d29fae35452
progzc <991186676@qq.com>
2022-03-29 23:07:52 +0800

    0088.合并两个有序数组(逆向双指针)

--- go-leetcode/classify_algorithm/double_pointer/leetcode_0088_merge-sorted-array/leetcode_0088_merge-sorted-array.go
+++ go-leetcode/classify_algorithm/double_pointer/leetcode_0088_merge-sorted-array/leetcode_0088_merge-sorted-array.go
@@ -16,7 +16,7 @@ func merge_1(nums1 []int, m int, nums2 []int, n int) {
 // merge_2 双指针
 // 时间复杂度: O(m+n)
 // 空间复杂度: O(m+n)
-// 思路: 充分利用讲个数组已经被排序的性质
+// 思路: 充分利用两个数组已经被排序的性质
 func merge_2(nums1 []int, m int, nums2 []int, n int) {
 	s := make([]int, 0, m+n)
 	p1, p2 := 0, 0
@@ -43,4 +43,37 @@ func merge_2(nums1 []int, m int, nums2 []int, n int) {
 		}
 	}
 	copy(nums1, s)
+}
+
+// merge_3 逆向双指针
+// 时间复杂度: O(m+n)
+// 空间复杂度: O(1)
+// 思路: 充分利用两个数组已经被排序的性质 + nums1的长度为m+n
+func merge_3(nums1 []int, m int, nums2 []int, n int) {
+	p1, p2, i := m-1, n-1, m+n-1
+
+	for p1 >= 0 && p2 >= 0 {
+		if nums1[p1] <= nums2[p2] {
+			nums1[i] = nums2[p2]
+			p2--
+		} else {
+			nums1[i] = nums1[p1]
+			p1--
+		}
+		i--
+	}
+	if p1 < 0 {
+		for p2 >= 0 {
+			nums1[i] = nums2[p2]
+			p2--
+			i--
+		}
+	}
+	if p2 < 0 {
+		for p1 >= 0 {
+			nums1[i] = nums1[p1]
+			p1--
+			i--
+		}
+	}
 }
