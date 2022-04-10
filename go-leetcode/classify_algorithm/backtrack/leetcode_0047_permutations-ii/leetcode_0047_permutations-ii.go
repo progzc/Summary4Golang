1f0a1c813745093528019e393a5bb3592b8eb4ac
progzc <991186676@qq.com>
2022-04-10 14:16:37 +0800

    0047.全排列 II

--- go-leetcode/classify_algorithm/backtrack/leetcode_0047_permutations-ii/leetcode_0047_permutations-ii.go
+++ go-leetcode/classify_algorithm/backtrack/leetcode_0047_permutations-ii/leetcode_0047_permutations-ii.go
@@ -1,13 +1,20 @@
 package leetcode_0047_permutations_ii
 
+import "sort"
+
 // 0047.全排列 II
 // https://leetcode-cn.com/problems/permutations-ii/
 // 题意：给定一个 含重复 数字的数组nums，返回其所有可能的 不重复的 全排列。你可以按任意顺序返回答案。
 
+// permuteUnique 深度遍历
+// 时间复杂度：O(n*n!)
+// 空间复杂度：O(2n)
 func permuteUnique(nums []int) [][]int {
 	if len(nums) == 0 {
 		return [][]int{}
 	}
+	// 必需先排序
+	sort.Ints(nums)
 	var (
 		ans    [][]int
 		used   = make([]bool, len(nums))
@@ -23,22 +30,23 @@ func permuteUnique(nums []int) [][]int {
 			ans = append(ans, append([]int(nil), output...))
 		}
 		for i := 0; i < len(nums); i++ {
-			// 剪枝
-			if used[i] || i > 0 && !used[i-1] && nums[i] == nums[i-1] {
+			// 剪枝：只可以选择未选择过的数
+			if used[i] {
+				continue
+			}
+			// 剪枝：只可以选择之前未选择过的不同的数
+			if i > 0 && !used[i-1] && nums[i] == nums[i-1] {
 				continue
 			}
 
-			// 前提：只可以选择未选择过的数
-			if !used[i] {
-				// 选择：选择第i个位置作为第idx个数
-				output = append(output, nums[i])
-				used[i] = true
-				// 递归：填下一个位置
-				dfs(idx+1, output)
-				// 回溯：在下一次选择之前，必须回撤销上一次的选择
-				used[i] = false
-				output = output[:len(output)-1]
-			}
+			// 选择：选择第i个位置作为第idx个数
+			output = append(output, nums[i])
+			used[i] = true
+			// 递归：填下一个位置
+			dfs(idx+1, output)
+			// 回溯：在下一次选择之前，必须回撤销上一次的选择
+			used[i] = false
+			output = output[:len(output)-1]
 		}
 	}
 	dfs(0, output)
