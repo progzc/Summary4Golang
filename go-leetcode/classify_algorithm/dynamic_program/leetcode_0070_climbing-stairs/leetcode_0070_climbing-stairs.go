6b652ba07599feadef82dbab9ff41ce027d7014c
progzc <991186676@qq.com>
2022-04-16 16:50:31 +0800

    0070.爬楼梯

---
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0070_climbing-stairs/leetcode_0070_climbing-stairs.go
@@ -0,0 +1,25 @@
+package leetcode_0070_climbing_stairs
+
+// 0070.爬楼梯
+// https://leetcode-cn.com/problems/climbing-stairs/
+
+// climbStairs 动态规划
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+// 思路：f(n)=f(n-1)+f(n-2)
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+
+	f1, f2 := 1, 2
+	ans := 0
+	for i := 3; i <= n; i++ {
+		ans = f1 + f2
+		f1, f2 = f2, ans
+	}
+	return ans
+}
