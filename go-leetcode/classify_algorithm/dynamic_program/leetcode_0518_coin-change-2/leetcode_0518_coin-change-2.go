63287270b9d3b9dc3b020364669cad243ada68a0
progzc <991186676@qq.com>
2022-04-17 21:15:13 +0800

    0518.零钱兑换 II

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_0518_coin-change-2/leetcode_0518_coin-change-2.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0518_coin-change-2/leetcode_0518_coin-change-2.go
@@ -19,3 +19,22 @@ func change(amount int, coins []int) int {
 	}
 	return dp[amount]
 }
+
+// change_2 动态规划(完全背包的组合问题)
+// 时间复杂度：O()
+// 空间复杂度：O()
+// 思路：动态规划
+//	定义：dp[i]表示金额之和等于i的硬币组合数
+func change_2(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	// 只有当不选取任何硬币时，金额之和才为0，因此只有1种硬币组合
+	dp[0] = 1
+	for _, coin := range coins {
+		for i := 1; i <= amount; i++ {
+			if i >= coin {
+				dp[i] = dp[i] + dp[i-coin]
+			}
+		}
+	}
+	return dp[amount]
+}
