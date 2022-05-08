6d7bee8ef39c17b2107415c31e0039ac2c540923
progzc <991186676@qq.com>
2022-05-08 09:46:12 +0800

    0725.分隔链表(优化)

--- go-leetcode/classify_algorithm/list/leetcode_0725_split-linked-list-in-parts/leetcode_0725_split-linked-list-in-parts.go
+++ go-leetcode/classify_algorithm/list/leetcode_0725_split-linked-list-in-parts/leetcode_0725_split-linked-list-in-parts.go
@@ -63,6 +63,44 @@ func splitListToParts(head *ListNode, k int) []*ListNode {
 // splitListToParts_2 一般思路（优化步骤）
 // 时间复杂度: O(n)
 // 空间复杂度: O(1)
+// 思路：记m,n:=count/k,count%k，则在分隔成的k个部分中，前n个部分的长度各为m+1，其余每个部分的长度各为m。
+// 注意事项：要简化代码
 func splitListToParts_2(head *ListNode, k int) []*ListNode {
-	// TODO
+	count := 0
+	for node := head; node != nil; node = node.Next {
+		count++
+	}
+
+	m, n := count/k, count%k
+	ans := make([]*ListNode, k) // 这样超出的部分就是nil了
+	for i, curr := 0, head; i < k && curr != nil; i++ {
+		ans[i] = curr
+		size := m
+		if i < n {
+			size++
+		}
+		// 注意事项：这里j=1开始,应为curr本身已经算成一个节点了
+		// 或者 for j := 0; j < size-1; j++
+		for j := 1; j < size; j++ {
+			curr = curr.Next
+		}
+		// 下面三行可以简写成：
+		// curr, curr.Next = curr.Next, nil
+		// 或：
+		// curr.Next, curr = nil, curr.Next
+
+		// 但是在力扣上分开写效率更高（表现在分开写消耗更低的内存）?
+		// 原因在于：golang给多个字段赋值时会为每个字段创建temp
+		// 	参见: http://www.javashuo.com/article/p-mmtmmmoz-ho.html
+		// 	例如：curr, curr.Next = curr.Next, nil
+		// 	等价于：
+		//		temp1 := curr
+		//		temp2 := curr.Next
+		//		curr = temp2
+		//		curr.Next = nil
+		next := curr.Next
+		curr.Next = nil // 这一步很关键，需要断开
+		curr = next
+	}
+	return ans
 }
