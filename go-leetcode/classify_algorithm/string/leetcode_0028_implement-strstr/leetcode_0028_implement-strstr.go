b23c22f7d5a5ed2d603b4c2460630712e8d84a35
progzc <991186676@qq.com>
2022-05-22 17:46:30 +0800

    0028.实现strStr()(KMP算法)

--- go-leetcode/classify_algorithm/string/leetcode_0028_implement-strstr/leetcode_0028_implement-strstr.go
+++ go-leetcode/classify_algorithm/string/leetcode_0028_implement-strstr/leetcode_0028_implement-strstr.go
@@ -5,6 +5,8 @@ import "strings"
 // 0028.实现strStr()
 // https://leetcode.cn/problems/implement-strstr/
 
+// KMP算法：https://zhuanlan.zhihu.com/p/83334559
+
 // strStr 直接使用API
 // 时间复杂度: O()
 // 空间复杂度: O()
@@ -21,8 +23,8 @@ func strStr(haystack string, needle string) int {
 }
 
 // strStr_2 暴力法
-// 时间复杂度: O((m-n)*n)
-// 空间复杂度: O(m-n)
+// 时间复杂度: O(m*n)
+// 空间复杂度: O(1)
 func strStr_2(haystack string, needle string) int {
 	m, n := len(haystack), len(needle)
 	if n == 0 {
@@ -41,6 +43,46 @@ func strStr_2(haystack string, needle string) int {
 		}
 		if find {
 			return i
+		}
+	}
+	return -1
+}
+
+// strStr_3 KMP
+// 时间复杂度: O(m+n)
+// 空间复杂度: O(n)
+// 思路：空间换时间
+func strStr_3(haystack string, needle string) int {
+	m, n := len(haystack), len(needle)
+	if n == 0 {
+		return 0
+	}
+	if m == 0 {
+		return -1
+	}
+
+	// 使用KMP算法进行搜索
+	// 步骤1：生成next数组
+	next := make([]int, n)
+	for i, j := 1, 0; i < n; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = next[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		next[i] = j
+	}
+	// 步骤2：利用next数组加速搜索
+	for i, j := 0, 0; i < m; i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = next[j-1]
+		}
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == n {
+			return i - n + 1
 		}
 	}
 	return -1
