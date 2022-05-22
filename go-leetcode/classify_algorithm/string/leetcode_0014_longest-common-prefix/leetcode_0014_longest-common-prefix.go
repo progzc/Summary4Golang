50a79a9c507821e83aeba7e2fc3e886f42b0697f
progzc <991186676@qq.com>
2022-05-22 10:19:49 +0800

    0014.最长公共前缀

---
+++ go-leetcode/classify_algorithm/string/leetcode_0014_longest-common-prefix/leetcode_0014_longest-common-prefix.go
@@ -0,0 +1,41 @@
+package leetcode_0014_longest_common_prefix
+
+import (
+	"bytes"
+	"strings"
+)
+
+// 0014.最长公共前缀
+// https://leetcode.cn/problems/longest-common-prefix/
+
+// longestCommonPrefix
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func longestCommonPrefix(strs []string) string {
+	i, ans := 0, bytes.Buffer{}
+	for {
+		for j := range strs {
+			if i >= len(strs[j]) || strs[0][i] != strs[j][i] {
+				return ans.String()
+			}
+		}
+		ans.WriteByte(strs[0][i])
+		i++
+	}
+}
+
+// longestCommonPrefix_2 零拷贝优化
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func longestCommonPrefix_2(strs []string) string {
+	i, ans := 0, strings.Builder{}
+	for {
+		for j := range strs {
+			if i >= len(strs[j]) || strs[0][i] != strs[j][i] {
+				return ans.String()
+			}
+		}
+		ans.WriteByte(strs[0][i])
+		i++
+	}
+}
