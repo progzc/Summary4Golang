8ca79fd3f21264d7f0ed47fb9b31fb482fc79457
progzc <991186676@qq.com>
2022-09-01 22:36:14 +0800

    0339. 嵌套列表权重和

---
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0727_minimum-window-subsequence/leetcode_0727_minimum-window-subsequence.go
@@ -0,0 +1,10 @@
+package leetcode_0727_minimum_window_subsequence
+
+// 0727. 最小窗口子序列
+// https://leetcode.cn/problems/minimum-window-subsequence/
+
+// minWindow 动态规划(前缀递推)
+// 思路：先计算包含 s2 的前缀子串的窗口，再根据前缀子串窗口不断拓展，找到包含整个字符串的窗口。
+func minWindow(s1 string, s2 string) string {
+
+}
