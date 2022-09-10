79e3ed2720f4118051c86751d158cf20fce20d76
progzc <991186676@qq.com>
2022-09-10 19:15:33 +0800

    最长连续序列

---
+++ go-leetcode/classify_algorithm/backtrack/leetcode_0562_longest-line-of-consecutive-one-in-matrix/leetcode_0562_longest-line-of-consecutive-one-in-matrix.go
@@ -0,0 +1,8 @@
+package leetcode_0562_longest_line_of_consecutive_one_in_matrix
+
+// 0562. 矩阵中最长的连续1线段
+// https://leetcode.cn/problems/longest-line-of-consecutive-one-in-matrix/
+
+func longestLine(mat [][]int) int {
+
+}
