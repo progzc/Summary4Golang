f17d36a7a9db1196e20dc2126840bb130b7e732f
progzc <991186676@qq.com>
2022-11-12 23:21:06 +0800

    0143. 重排链表

---
+++ go-leetcode/classify_algorithm/list/leetcode_0143_reorder-list/leetcode_0143_reorder-list.go
@@ -0,0 +1,30 @@
+package leetcode_0143_reorder_list
+
+// 0143. 重排链表
+// https://leetcode.cn/problems/reorder-list/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// reorderList 递归
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func reorderList(head *ListNode) {
+	var queue []*ListNode
+	for head != nil {
+		queue = append(queue, head)
+		head = head.Next
+	}
+
+	for len(queue) > 2 {
+		first, last := queue[0], queue[len(queue)-1]
+		queue = queue[1 : len(queue)-1]
+		last.Next = first.Next
+		first.Next = last
+		if len(queue) > 0 {
+			queue[len(queue)-1].Next = nil
+		}
+	}
+}
