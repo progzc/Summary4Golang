257341d07889e351f111b88ac5163b318ba0951a
progzc <991186676@qq.com>
2022-11-15 12:26:23 +0800

    fix:0494.目标和

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_0494_target-sum/leetcode_0494_target-sum.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0494_target-sum/leetcode_0494_target-sum.go
@@ -8,7 +8,7 @@ package leetcode_0494_target_sum
 // 空间复杂度: O(T)
 // 思路：转化为如下的0/1背包问题
 // 数组和sum,目标和target, 正数和x,负数和y,则x+y=sum,x-y=target,
-// 那么x=(target+sum)/2=newTarget,y=(target-sum)/2
+// 那么x=(target+sum)/2=newTarget,y=(sum-target)/2
 // ==> 选nums里的数得到target的种数
 // 特点：0-1背包不考虑元素顺序的组合问题
 func findTargetSumWays(nums []int, target int) int {
@@ -16,13 +16,13 @@ func findTargetSumWays(nums []int, target int) int {
 	for _, num := range nums {
 		sum += num
 	}
-	// 注意点1: 由于y是负数,所以y=(target-sum)/2<0==>sum<target
+	// 注意点1: 由于-y是非正数,所以-y=(target-sum)/2<=0==>sum>=target
 	if (sum+target)%2 != 0 || sum < target {
 		return 0
 	}
 	newTarget := (sum + target) / 2
 	dp := make([]int, newTarget+1)
-	// 注意点2: 当没有任何元素可以选取时，元素和只能是0,应的方案数是1
+	// 注意点2: 当没有任何元素可以选取时，元素和只能是0,对应的方案数是1
 	dp[0] = 1
 	for _, num := range nums {
 		for j := newTarget; j >= num; j-- {
