52aacac27319ba2b5b7f2e231c5844cb873bc32f
progzc <991186676@qq.com>
2022-11-17 13:48:47 +0800

    add:0416.分割等和子集（二维动态规划）

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_0416_partition-equal-subset-sum/leetcode_0416_partition-equal-subset-sum_test.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0416_partition-equal-subset-sum/leetcode_0416_partition-equal-subset-sum_test.go
@@ -43,6 +43,50 @@ func canPartition(nums []int) bool {
 	return dp[target]
 }
 
+// canPartition_2 动态规划（0/1背包问题,二维动态规划）
+// 时间复杂度：O(N*C),其中N为数组元素的个数，C是数组元素的和的一半
+// 空间复杂度：O(C)
+// 思路：
+//	a.数组的和必须为偶数
+//	b.可以转换为0/1背包问题：是否可以从输入数组中挑选出一些正整数，使得这些数的和等于整个数组元素的和的一半。
+// 状态: dp[i][j]表示是否可以从前i个输入数组中挑选出一些正整数，使得这些数的和等于j
+func canPartition_2(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if sum%2 == 1 {
+		return false
+	}
+
+	target := sum / 2
+	n := len(nums)
+	dp := make([][]bool, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = make([]bool, target+1)
+	}
+
+	// 初始状态
+	for j := 0; j < target+1; j++ {
+		if j == 0 {
+			dp[0][j] = true
+		} else {
+			dp[0][j] = false
+		}
+	}
+
+	for i := 1; i < n+1; i++ {
+		for j := 0; j < target+1; j++ {
+			if nums[i-1] > j {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
+			}
+		}
+	}
+	return dp[n][target]
+}
+
 //[true	true false false false false false false false false false false false false false false false]
 //[true	true false false false true true false false false false false false false false false false]
 //[true	true false false false true true false false false false true true false false false true]
