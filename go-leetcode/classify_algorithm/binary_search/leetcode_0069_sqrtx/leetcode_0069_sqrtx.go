158c55078b274a808ff7423c677e7c44d8bbde72
progzc <991186676@qq.com>
2022-11-25 22:25:54 +0800

    69. x 的平方根

---
+++ go-leetcode/classify_algorithm/binary_search/leetcode_0069_sqrtx/leetcode_0069_sqrtx.go
@@ -0,0 +1,22 @@
+package leetcode_0069_sqrtx
+
+// 69. x 的平方根
+// https://leetcode.cn/problems/sqrtx/
+
+// mySqrt 二分查找
+// 时间复杂度: O(log(n))
+// 空间复杂度: O(1)
+func mySqrt(x int) int {
+	l, r := 0, x
+	ans := -1
+	for l <= r {
+		mid := l + (r-l)>>2
+		if mid*mid <= x {
+			ans = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ans
+}
