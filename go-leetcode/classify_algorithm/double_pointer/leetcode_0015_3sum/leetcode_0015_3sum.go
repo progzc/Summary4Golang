8b018d35a771f554cce0c6a8b98861a68f3532d5
progzc <991186676@qq.com>
2022-11-27 21:33:30 +0800

    0015.三数之和

--- go-leetcode/classify_algorithm/double_pointer/leetcode_0015_3sum/leetcode_0015_3sum.go
+++ go-leetcode/classify_algorithm/double_pointer/leetcode_0015_3sum/leetcode_0015_3sum.go
@@ -15,34 +15,38 @@ import (
 // 2. 排序之后采用三重循环，要保证每重循环 相邻两次枚举的元素不能相同
 // 双指针：当随着第一个元素的递增,第二个元素是递减的，就可以考虑使用 双指针
 func threeSum(nums []int) [][]int {
+	var ans [][]int
 	n := len(nums)
-	sort.Ints(nums)
-	ans := [][]int{}
+	if n < 3 {
+		return ans
+	}
 
+	sort.Ints(nums)
 	// 枚举a
-	for first := 0; first < n; first++ {
+	for i := 0; i < n-2; i++ {
 		// 需要和上一次枚举的数不同(但是第一次循环必须执行)
-		if first > 0 && nums[first] == nums[first-1] {
+		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-		third := n - 1
-		target := -1 * nums[first]
+		target := -1 * nums[i]
+		k := n - 1
 		// 枚举b
-		for second := first + 1; second < n; second++ {
+		for j := i + 1; j < n-1; j++ {
 			// 需要和上一次枚举的数不同(但是第一次循环必须执行)
-			if second > first+1 && nums[second] == nums[second-1] {
+			if j > i+1 && nums[j] == nums[j-1] {
 				continue
 			}
 			// 保证b在c左侧
-			for second < third && nums[second]+nums[third] > target {
-				third--
+			for j < k && nums[j]+nums[k] > target {
+				k--
 			}
 			// 提前退出
-			if second == third {
+			if j == k {
 				break
 			}
-			if nums[second]+nums[third] == target {
-				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			// 添加结果
+			if nums[j]+nums[k] == target {
+				ans = append(ans, []int{nums[i], nums[j], nums[k]})
 			}
 		}
 	}
