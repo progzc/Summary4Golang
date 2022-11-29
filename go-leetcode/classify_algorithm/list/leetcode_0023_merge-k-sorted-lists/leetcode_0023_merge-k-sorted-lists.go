d8ca4ec0d3531b7988aba9660799f0241ba0128f
progzc <991186676@qq.com>
2022-11-29 11:06:41 +0800

    0023.合并K个升序链表

--- go-leetcode/classify_algorithm/list/leetcode_0023_merge-k-sorted-lists/leetcode_0023_merge-k-sorted-lists.go
+++ go-leetcode/classify_algorithm/list/leetcode_0023_merge-k-sorted-lists/leetcode_0023_merge-k-sorted-lists.go
@@ -50,41 +50,38 @@ func mergeKLists(lists []*ListNode) *ListNode {
 // 空间复杂度: O(log(k))
 // 思路：由合并两个链表==>拓展想到使用分治
 func mergeKLists_2(lists []*ListNode) *ListNode {
-	if len(lists) == 0 {
+	n := len(lists)
+	if n == 0 {
 		return nil
 	}
-	return merge(lists, 0, len(lists)-1)
+	if n == 1 {
+		return lists[0]
+	}
+
+	l, r := 0, n-1
+	mid := l + (r-l)>>2
+	return merge(mergeKLists_2(lists[l:mid+1]), mergeKLists_2(lists[mid+1:]))
 }
 
-func merge(lists []*ListNode, left, right int) *ListNode {
-	if left == right {
-		return lists[left]
-	}
-	if left > right {
-		return nil
-	}
-	mid := (left + right) >> 1
-	return mergeTwoLists(merge(lists, left, mid), merge(lists, mid+1, right))
-
-}
-
-func mergeTwoLists(list1, list2 *ListNode) *ListNode {
-	prev := new(ListNode)
-	sentry := prev
+func merge(list1, list2 *ListNode) *ListNode {
+	dummy := &ListNode{0, nil}
+	cur := dummy
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
-			prev.Next = list1
+			cur.Next = list1
 			list1 = list1.Next
 		} else {
-			prev.Next = list2
+			cur.Next = list2
 			list2 = list2.Next
 		}
-		prev = prev.Next
+		cur = cur.Next
 	}
-	if list1 == nil {
-		prev.Next = list2
-	} else {
-		prev.Next = list1
+
+	if list1 != nil {
+		cur.Next = list1
 	}
-	return sentry.Next
+	if list2 != nil {
+		cur.Next = list2
+	}
+	return dummy.Next
 }
