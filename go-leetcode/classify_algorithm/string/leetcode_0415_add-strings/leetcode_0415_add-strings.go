2cd66fd068975a57b4453cd8a2c00125a449d730
progzc <991186676@qq.com>
2022-11-29 22:42:05 +0800

    0415.字符串相加

--- go-leetcode/classify_algorithm/string/leetcode_0415_add-strings/leetcode_0415_add-strings.go
+++ go-leetcode/classify_algorithm/string/leetcode_0415_add-strings/leetcode_0415_add-strings.go
@@ -1,6 +1,9 @@
 package leetcode_0415_add_strings
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 // 0415.字符串相加
 // https://leetcode-cn.com/problems/add-strings/
@@ -26,6 +29,34 @@ func addStrings(num1 string, num2 string) string {
 	}
 	if carry > 0 {
 		ans = strconv.Itoa(carry) + ans
+	}
+	return ans
+}
+
+// addStrings_2 模拟
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func addStrings_2(num1 string, num2 string) string {
+	n1, n2 := len(num1)-1, len(num2)-1
+	carry := 0
+	var ans string
+	for n1 >= 0 || n2 >= 0 {
+		var x, y int
+		if n1 >= 0 {
+			x, _ = strconv.Atoi(string(num1[n1]))
+			n1--
+		}
+		if n2 >= 0 {
+			y, _ = strconv.Atoi(string(num2[n2]))
+			n2--
+		}
+		tmp := x + y + carry
+		cur := tmp % 10
+		carry = tmp / 10
+		ans = fmt.Sprintf("%d", cur) + ans
+	}
+	if carry > 0 {
+		ans = fmt.Sprintf("%d", carry) + ans
 	}
 	return ans
 }
