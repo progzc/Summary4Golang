5f677d03e20c8bb9b878c8679b044c8134bdcc16
progzc <991186676@qq.com>
2022-12-08 23:42:02 +0800

    623. 在二叉树中增加一行

---
+++ go-leetcode/classify_algorithm/tree/leetcode_0623_add-one-row-to-tree/leetcode_0623_add-one-row-to-tree.go
@@ -0,0 +1,45 @@
+package leetcode_0623_add_one_row_to_tree
+
+// 623. 在二叉树中增加一行
+// https://leetcode.cn/problems/add-one-row-to-tree/
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// addOneRow bfs
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func addOneRow(root *TreeNode, val int, depth int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if depth == 1 {
+		return &TreeNode{Val: val, Left: root}
+	}
+
+	var stack []*TreeNode
+	stack = append(stack, root)
+	for i := 0; i < depth-2; i++ {
+		n := len(stack)
+		for j := 0; j < n; j++ {
+			node := stack[0]
+			stack = stack[1:]
+			if node.Left != nil {
+				stack = append(stack, node.Left)
+			}
+			if node.Right != nil {
+				stack = append(stack, node.Right)
+			}
+		}
+	}
+
+	for _, node := range stack {
+		node.Left = &TreeNode{Val: val, Left: node.Left}
+		node.Right = &TreeNode{Val: val, Right: node.Right}
+	}
+	return root
+}
