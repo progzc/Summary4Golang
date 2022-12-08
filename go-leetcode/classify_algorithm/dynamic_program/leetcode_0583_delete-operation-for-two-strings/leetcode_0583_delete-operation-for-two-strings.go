04d313f469ea9b50173d57ad697b570af3ae628b
progzc <991186676@qq.com>
2022-12-08 15:54:36 +0800

    583. 两个字符串的删除操作

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_0583_delete-operation-for-two-strings/leetcode_0583_delete-operation-for-two-strings.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0583_delete-operation-for-two-strings/leetcode_0583_delete-operation-for-two-strings.go
@@ -1,7 +1,7 @@
-package leetcode_0072_edit_distance
+package leetcode_0583_delete_operation_for_two_strings
 
-// 0072.编辑距离
-// https://leetcode-cn.com/problems/edit-distance/
+// 583. 两个字符串的删除操作
+// https://leetcode.cn/problems/delete-operation-for-two-strings/
 
 // 注意:
 // 【583.两个字符串的删除操作】：只允许插入
@@ -14,11 +14,9 @@ package leetcode_0072_edit_distance
 //	本质不同的操作只有三种：
 //		a.在单词A中插入一个字符(等价于在单词B中删除一个字符)
 //		b.在单词B中插入一个字符(等价于在单词A中删除一个字符)
-//		c.修改单词A的一个字符
-//	动态规划公式：用D[i][j]表示A的前i个字母和B的前j个字母之间的编辑距离。则
-//		若A和B的最后一个字母不同：D[i][j] = min{ D[i-1][j]+1, D[i][j-1]+1, D[i-1][j-1]+1 }
+//	动态规划公式：用D[i][j]表示A的前i个字母和B的前j个字母之间的最小步数。则
+//		若A和B的最后一个字母不同：D[i][j] = min{ D[i-1][j]+1, D[i][j-1]+1, D[i-1][j-1]+2 }
 //		若A和B的最后一个字母相同：D[i][j] = min{ D[i-1][j]+1, D[i][j-1]+1, D[i-1][j-1] }
-// 扩展：编辑距离算法被数据科学家广泛应用，是用作机器翻译和语音识别评价标准的基本算法。
 func minDistance(word1 string, word2 string) int {
 	n, m := len(word1), len(word2)
 	// 若有一个字符串为空串
@@ -42,10 +40,10 @@ func minDistance(word1 string, word2 string) int {
 		for j := 1; j < m+1; j++ {
 			s1 := dp[i-1][j] + 1
 			s2 := dp[i][j-1] + 1
-			// 注意这里与【583.两个字符串的删除操作】的区别
-			s3 := dp[i-1][j-1] + 1
+			// 注意这里与【72.编辑距离】的区别
+			s3 := dp[i-1][j-1] + 2
 			if word1[i-1] == word2[j-1] {
-				s3 -= 1
+				s3 -= 2
 			}
 			dp[i][j] = min(s1, min(s2, s3))
 		}
