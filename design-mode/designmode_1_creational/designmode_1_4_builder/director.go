101875412a6f317e2a3312c2abfcb914e29cb72b
progzczhao <progzczhao@tencent.com>
2023-08-06 12:37:13 +0800

    建造者模式

---
+++ design-mode/designmode_1_creational/designmode_1_4_builder/director.go
@@ -0,0 +1,22 @@
+package designmode_1_4_builder
+
+type Director struct {
+	builder IBuilder
+}
+
+func newDirector(b IBuilder) *Director {
+	return &Director{
+		builder: b,
+	}
+}
+
+func (d *Director) setBuilder(b IBuilder) {
+	d.builder = b
+}
+
+func (d *Director) buildHouse() House {
+	d.builder.setDoorType()
+	d.builder.setWindowType()
+	d.builder.setNumFloor()
+	return d.builder.getHouse()
+}
