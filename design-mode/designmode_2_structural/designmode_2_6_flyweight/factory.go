661bc7cf2012b2baaabd6a9bacf80af15282fda8
progzczhao <progzczhao@tencent.com>
2023-08-06 22:38:24 +0800

    享元模式

---
+++ design-mode/designmode_2_structural/designmode_2_6_flyweight/factory.go
@@ -0,0 +1,41 @@
+package designmode_2_6_flyweight
+
+import "fmt"
+
+const (
+	//TerroristDressType terrorist dress type
+	TerroristDressType = "tDress"
+	//CounterTerrroristDressType terrorist dress type
+	CounterTerrroristDressType = "ctDress"
+)
+
+var (
+	dressFactorySingleInstance = &DressFactory{
+		dressMap: make(map[string]Dress),
+	}
+)
+
+type DressFactory struct {
+	dressMap map[string]Dress
+}
+
+func (d *DressFactory) getDressByType(dressType string) (Dress, error) {
+	if d.dressMap[dressType] != nil {
+		return d.dressMap[dressType], nil
+	}
+
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	if dressType == CounterTerrroristDressType {
+		d.dressMap[dressType] = newCounterTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+
+	return nil, fmt.Errorf("Wrong dress type passed")
+}
+
+func getDressFactorySingleInstance() *DressFactory {
+	return dressFactorySingleInstance
+}
