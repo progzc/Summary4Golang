53147003834d80330197d7eb1c19ad6767b513e5
progzczhao <progzczhao@tencent.com>
2023-08-06 15:09:42 +0800

    组合模式

---
+++ design-mode/designmode_2_structural/designmode_2_3_composite/main_test.go
@@ -0,0 +1,23 @@
+package designmode_2_3_composite
+
+import "testing"
+
+func Test_main(t *testing.T) {
+	file1 := &File{name: "File1"}
+	file2 := &File{name: "File2"}
+	file3 := &File{name: "File3"}
+
+	folder1 := &Folder{
+		name: "Folder1",
+	}
+	folder1.add(file1)
+
+	folder2 := &Folder{
+		name: "Folder2",
+	}
+	folder2.add(file2)
+	folder2.add(file3)
+	folder2.add(folder1)
+
+	folder2.search("rose")
+}
