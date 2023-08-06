12f06778533ba7d7293d0ed726f44ef678195045
progzczhao <progzczhao@tencent.com>
2023-08-06 15:50:46 +0800

    外观模式

---
+++ design-mode/designmode_2_structural/designmode_2_5_facade/main_test.go
@@ -0,0 +1,23 @@
+package designmode_2_5_facade
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
+}
