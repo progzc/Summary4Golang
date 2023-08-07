03d1aaacfcb7a6ec5fc57af090ea6d0d50f11aed
progzczhao <progzczhao@tencent.com>
2023-08-07 09:36:50 +0800

    模板方法模式

---
+++ design-mode/designmode_3_behavioral/designmode_3_9_template-method/template.go
@@ -0,0 +1,16 @@
+package designmode_3_9_template_method
+
+type Otp struct {
+	iOtp IOtp
+}
+
+func (o *Otp) genAndSendOTP(otpLength int) error {
+	otp := o.iOtp.genRandomOTP(otpLength)
+	o.iOtp.saveOTPCache(otp)
+	message := o.iOtp.getMessage(otp)
+	err := o.iOtp.sendNotification(message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
