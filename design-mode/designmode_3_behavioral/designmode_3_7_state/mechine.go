919585f99fbe4b713d0ba04c829ee1a27470d487
progzczhao <progzczhao@tencent.com>
2023-08-07 09:06:55 +0800

    状态模式

---
+++ design-mode/designmode_3_behavioral/designmode_3_7_state/mechine.go
@@ -0,0 +1,66 @@
+package designmode_3_7_state
+
+import "fmt"
+
+type VendingMachine struct {
+	hasItem       State
+	itemRequested State
+	hasMoney      State
+	noItem        State
+
+	currentState State
+
+	itemCount int
+	itemPrice int
+}
+
+func newVendingMachine(itemCount, itemPrice int) *VendingMachine {
+	v := &VendingMachine{
+		itemCount: itemCount,
+		itemPrice: itemPrice,
+	}
+	hasItemState := &HasItemState{
+		vendingMachine: v,
+	}
+	itemRequestedState := &ItemRequestedState{
+		vendingMachine: v,
+	}
+	hasMoneyState := &HasMoneyState{
+		vendingMachine: v,
+	}
+	noItemState := &NoItemState{
+		vendingMachine: v,
+	}
+
+	v.setState(hasItemState)
+	v.hasItem = hasItemState
+	v.itemRequested = itemRequestedState
+	v.hasMoney = hasMoneyState
+	v.noItem = noItemState
+	return v
+}
+
+func (v *VendingMachine) requestItem() error {
+	return v.currentState.requestItem()
+}
+
+func (v *VendingMachine) addItem(count int) error {
+	return v.currentState.addItem(count)
+}
+
+func (v *VendingMachine) insertMoney(money int) error {
+	return v.currentState.insertMoney(money)
+}
+
+func (v *VendingMachine) dispenseItem() error {
+	return v.currentState.dispenseItem()
+}
+
+func (v *VendingMachine) setState(s State) {
+	v.currentState = s
+}
+
+func (v *VendingMachine) incrementItemCount(count int) {
+	fmt.Printf("Adding %d items\n", count)
+	v.itemCount = v.itemCount + count
+}
