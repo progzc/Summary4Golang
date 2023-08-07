d9db5be768d14f56c3eb5cdd12cfbfe2699a70d1
progzczhao <progzczhao@tencent.com>
2023-08-07 09:59:55 +0800

    访问者模式

---
+++ design-mode/designmode_3_behavioral/designmode_3_10_visitor/element.go
@@ -0,0 +1,43 @@
+package designmode_3_10_visitor
+
+type Shape interface {
+	getType() string
+	accept(Visitor)
+}
+
+type Square struct {
+	side int
+}
+
+func (s *Square) accept(v Visitor) {
+	v.visitForSquare(s)
+}
+
+func (s *Square) getType() string {
+	return "Square"
+}
+
+type Circle struct {
+	radius int
+}
+
+func (c *Circle) accept(v Visitor) {
+	v.visitForCircle(c)
+}
+
+func (c *Circle) getType() string {
+	return "Circle"
+}
+
+type Rectangle struct {
+	l int
+	b int
+}
+
+func (t *Rectangle) accept(v Visitor) {
+	v.visitForrectangle(t)
+}
+
+func (t *Rectangle) getType() string {
+	return "rectangle"
+}
