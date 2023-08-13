4196650bab3e2e1b09ef643dfb8c472d4ce422f7
progzczhao <progzczhao@tencent.com>
2023-08-13 18:24:09 +0800

    令牌桶限流

---
+++ go-tool/go.mod
@@ -0,0 +1,8 @@
+module github.com/progzc/Summary4Golang/go-tool
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+	golang.org/x/time v0.3.0
+)
---
+++ go-tool/lfu/lfu.go
@@ -0,0 +1,24 @@
+package lfu
+
+// 0460.LFU缓存
+// https://leetcode-cn.com/problems/lfu-cache/
+// LFU：Least Frequently Used，最不经常使用
+
+// 思路：双map
+
+type LFUCache struct {
+}
+
+func Constructor(capacity int) LFUCache {
+	// TODO
+	return LFUCache{}
+}
+
+func (this *LFUCache) Get(key int) int {
+	// TODO
+	return -1
+}
+
+func (this *LFUCache) Put(key int, value int) {
+
+}
