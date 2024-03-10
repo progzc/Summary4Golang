2f9fa4108b9ef22a93abbd7f68688f35cf125c40
progzc <991186676@qq.com>
2024-03-10 15:29:50 +0800

    0143. 重排链表

--- go-leetcode/classify_algorithm/list/leetcode_0143_reorder-list/leetcode_0143_reorder-list.go
+++ go-leetcode/classify_algorithm/list/leetcode_0143_reorder-list/leetcode_0143_reorder-list.go
@@ -26,3 +26,29 @@ func reorderList(head *ListNode) {
 		queue[len(queue)-1].Next = nil
 	}
 }
+
+// reorderList_2 列表
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func reorderList_2(head *ListNode) {
+	cur := head
+	var stack []*ListNode
+	for cur != nil {
+		stack = append(stack, cur)
+		cur = cur.Next
+	}
+
+	for len(stack) >= 3 {
+		first := stack[0]
+		next := first.Next
+
+		before := stack[len(stack)-2]
+		last := stack[len(stack)-1]
+
+		first.Next = last
+		last.Next = next
+		before.Next = nil
+		stack = stack[1 : len(stack)-1]
+	}
+	return
+}
