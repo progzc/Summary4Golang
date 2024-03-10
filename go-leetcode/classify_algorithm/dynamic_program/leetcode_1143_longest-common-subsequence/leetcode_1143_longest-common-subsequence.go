7e3b840421510be8f904d44a205fd0cc98efbf58
progzc <991186676@qq.com>
2024-03-10 18:02:44 +0800

    1143.最长公共子序列

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_1143_longest-common-subsequence/leetcode_1143_longest-common-subsequence.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_1143_longest-common-subsequence/leetcode_1143_longest-common-subsequence.go
@@ -27,6 +27,54 @@ func longestCommonSubsequence(text1 string, text2 string) int {
 	return dp[m][n]
 }
 
+// longestCommonSubsequence_2 二维动态规划
+// 时间复杂度: O(m*n)
+// 空间复杂度: O(m*n)
+// 思路：典型的动态规划
+func longestCommonSubsequence_2(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	// dp[i][j]: 表示text1[:i]与text2[:j]的最长公共子序列的长度
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	// 初始化:
+	for i := 0; i < m; i++ {
+		if i == 0 {
+			if text1[i] == text2[0] {
+				dp[i][0] = 1
+			}
+		} else {
+			if text1[i] == text2[0] || dp[i-1][0] > 0 {
+				dp[i][0] = 1
+			}
+		}
+	}
+
+	for j := 0; j < n; j++ {
+		if j == 0 {
+			if text1[0] == text2[j] {
+				dp[0][j] = 1
+			}
+		} else {
+			if text1[0] == text2[j] || dp[0][j-1] > 0 {
+				dp[0][j] = 1
+			}
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if text1[i] == text2[j] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+
 func max(x, y int) int {
 	if x > y {
 		return x
