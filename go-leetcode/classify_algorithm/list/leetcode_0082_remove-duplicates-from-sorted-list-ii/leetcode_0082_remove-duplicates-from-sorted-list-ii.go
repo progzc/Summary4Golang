ec77a15990a82f763643e66f3b7f4bbcf4159a7b
progzc <991186676@qq.com>
2024-03-11 22:14:56 +0800

    0082. 删除排序链表中的重复元素 II

--- go-leetcode/classify_algorithm/list/leetcode_0082_remove-duplicates-from-sorted-list-ii/leetcode_0082_remove-duplicates-from-sorted-list-ii.go
+++ go-leetcode/classify_algorithm/list/leetcode_0082_remove-duplicates-from-sorted-list-ii/leetcode_0082_remove-duplicates-from-sorted-list-ii.go
@@ -17,11 +17,11 @@ func deleteDuplicates(head *ListNode) *ListNode {
 	dummy := &ListNode{math.MinInt32, head}
 	cur := dummy.Next
 	pre := dummy
-	for cur != nil && cur.Next != nil {
-		if cur.Val != cur.Next.Val {
+	for cur != nil && cur.Next != nil { // 少于两个节点直接结束
+		if cur.Val != cur.Next.Val { // 两个节点值不同，直接向下遍历
 			pre = cur
 			cur = cur.Next
-		} else {
+		} else { // 两个节点值相同，找到下一个不同的节点
 			for cur.Next != nil && cur.Val == cur.Next.Val {
 				cur = cur.Next
 			}
@@ -30,5 +30,31 @@ func deleteDuplicates(head *ListNode) *ListNode {
 		}
 	}
 
+	return dummy.Next
+}
+
+// deleteDuplicates_2 顺序遍历
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func deleteDuplicates_2(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	cur := dummy.Next
+	for cur != nil && cur.Next != nil {
+		first := cur
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur = cur.Next
+		}
+		if cur.Next == nil {
+			pre.Next = nil
+		} else {
+			if first == cur {
+				pre = cur
+			} else {
+				pre.Next = cur.Next
+			}
+		}
+		cur = cur.Next
+	}
 	return dummy.Next
 }
