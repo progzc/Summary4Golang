8d7e3837ce7afd94b376b35d9e347d8821294cc2
progzczhao <zcprog@foxmail.com>
2024-09-26 17:58:27 +0800

    0162. 寻找峰值

--- go-leetcode/classify_algorithm/binary_search/leetcode_0162_find-peak-element/leetcode_0162_find-peak-element.go
+++ go-leetcode/classify_algorithm/binary_search/leetcode_0162_find-peak-element/leetcode_0162_find-peak-element.go
@@ -6,9 +6,18 @@ package leetcode_0162_find_peak_element
 // findPeakElement 二分法
 // 时间复杂度: O(log(n))
 // 空间复杂度: O(1)
+// 思路：在题目描述中出现了 nums[-1] = nums[n] = -∞，这就代表着 只要数组中存在一个元素比相邻元素大，那么沿着它一定可以找到一个峰值。
 func findPeakElement(nums []int) int {
-	// todo
-	return 0
+	left, right := 0, len(nums)-1
+	for left < right { // 不能取等于，不然就会出现nums[mid+1]越界的情况
+		mid := left + (right-left)/2
+		if nums[mid] > nums[mid+1] { // 不会出现等于的情况，否则就无法进行搜索了。
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
 }
 
 // findPeakElement_2 寻找最大值
