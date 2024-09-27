0e7d358b35de13990358f37e1785cf601d441a1f
progzczhao <zcprog@foxmail.com>
2024-09-27 11:59:31 +0800

    0239.滑动窗口最大值

--- go-leetcode/classify_algorithm/monotone_queue/leetcode_0239_sliding-window-maximum/leetcode_0239_sliding-window-maximum.go
+++ go-leetcode/classify_algorithm/monotone_queue/leetcode_0239_sliding-window-maximum/leetcode_0239_sliding-window-maximum.go
@@ -7,6 +7,7 @@ package leetcode_0239_sliding_window_maximum
 // 时间复杂度: O(n)
 // 空间复杂度: O(k)
 // 思路:
+//
 //	我们可以使用一个队列存储所有还没有被移除的下标。在队列中，这些下标按照从小到大的顺序被存储，并且它们在数组 nums 中对应的值是严格单调递减的。
 //	简言之, 队列中随着下标递增, 值递减。
 func maxSlidingWindow(nums []int, k int) []int {
