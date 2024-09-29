b8d78edd00e1e7fe03c4a4bb7913c3634d286f60
progzczhao <zcprog@foxmail.com>
2024-09-29 18:35:02 +0800

    go语言探索

---
+++ read-note/go-explore/abc/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	age int
+}
+
+func (p Person) howOld() int {
+	return p.age
+}
+
+func (p *Person) growUp() {
+	p.age += 1
+}
+
+func main() {
+	// qcrao 是值类型
+	qcrao := Person{age: 18}
+
+	// 值类型 调用接收者也是值类型的方法
+	fmt.Println(qcrao.howOld())
+
+	// 值类型 调用接收者是指针类型的方法
+	qcrao.growUp()
+	fmt.Println(qcrao.howOld())
+
+	// ----------------------
+
+	// stefno 是指针类型
+	stefno := &Person{age: 100}
+
+	// 指针类型 调用接收者是值类型的方法
+	fmt.Println(stefno.howOld())
+
+	// 指针类型 调用接收者也是指针类型的方法
+	stefno.growUp()
+	fmt.Println(stefno.howOld())
+}
