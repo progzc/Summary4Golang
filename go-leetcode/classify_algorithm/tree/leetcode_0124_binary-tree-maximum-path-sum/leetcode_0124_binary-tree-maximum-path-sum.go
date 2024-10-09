ea53cbe9dcf427307298a54780f7240c96408dfc
progzczhao <zcprog@foxmail.com>
2024-10-09 14:25:48 +0800

    124. 二叉树中的最大路径和

--- go-leetcode/classify_algorithm/tree/leetcode_0124_binary-tree-maximum-path-sum/leetcode_0124_binary-tree-maximum-path-sum.go
+++ go-leetcode/classify_algorithm/tree/leetcode_0124_binary-tree-maximum-path-sum/leetcode_0124_binary-tree-maximum-path-sum.go
@@ -2,7 +2,7 @@ package leetcode_0124_binary_tree_maximum_path_sum
 
 import "math"
 
-// 124. 二叉树中的最大路径和
+// 124. 二叉树中的最大路径和🌟
 // https://leetcode.cn/problems/binary-tree-maximum-path-sum/
 
 // 同以下题目类似:
@@ -19,9 +19,10 @@ type TreeNode struct {
 // 时间复杂度: O(n)
 // 空间复杂度: O(n)
 // 思路:
-//	对于任意一个节点, 如果最大和路径包含该节点, 那么只可能是两种情况:
-//	  1.其左右子树中所构成的和路径值较大的那个加上该节点的值后向父节点回溯构成最大路径
-//	  2.左右子树都在最大路径中, 加上该节点的值构成了最终的最大路径
+// 对于任意一个节点, 如果最大和路径包含该节点, 那么只可能是两种情况:
+// 1.其左右子树中所构成的和路径值较大的那个加上该节点的值后向父节点回溯构成最大路径
+// 2.左右子树都在最大路径中, 加上该节点的值构成了最终的最大路径
+// 特殊示例：root = [-3], 输出结果-3
 func maxPathSum(root *TreeNode) int {
 	var (
 		dfs func(root *TreeNode) int
@@ -41,6 +42,35 @@ func maxPathSum(root *TreeNode) int {
 		ans = max(ans, root.Val+left+right)
 		// 返回经过root的单边最大分支给当前root的父节点计算使用
 		return root.Val + max(left, right)
+	}
+	dfs(root)
+	return ans
+}
+
+// maxPathSum_2
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+// 思路:
+// 对于任意一个节点, 如果最大和路径包含该节点, 那么只可能是两种情况:
+// 1.其左右子树中所构成的和路径值较大的那个加上该节点的值后向父节点回溯构成最大路径
+// 2.左右子树都在最大路径中, 加上该节点的值构成了最终的最大路径
+// 特殊示例：root = [-3], 输出结果-3
+func maxPathSum_2(root *TreeNode) int {
+	var (
+		dfs func(root *TreeNode) int
+		ans = math.MinInt32
+	)
+
+	// dfs 定义从root节点为起始点出发的最大路径和
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+		left := dfs(root.Left)
+		right := dfs(root.Right)
+		// 可能情况：左右都不选；只选左边；只选右边；同时选左右
+		ans = max(ans, max(max(max(root.Val, root.Val+left), root.Val+right), root.Val+left+right))
+		return max(root.Val, max(left+root.Val, right+root.Val))
 	}
 	dfs(root)
 	return ans
