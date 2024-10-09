3e8ff3926996f2a40fac499b9c97e91e08040d34
progzczhao <zcprog@foxmail.com>
2024-10-09 15:54:54 +0800

    0155.最小栈

--- go-leetcode/classify_algorithm/stack_queue/leetcode_0155_min-stack/leetcode_0155_min-stack.go
+++ go-leetcode/classify_algorithm/stack_queue/leetcode_0155_min-stack/leetcode_0155_min-stack.go
@@ -1,6 +1,6 @@
 package leetcode_0155_min_stack
 
-// 0155.最小栈
+// 0155.最小栈🌟
 // https://leetcode-cn.com/problems/min-stack/
 
 // MinStack 两个栈
@@ -45,3 +45,39 @@ func (this *MinStack) Top() int {
 func (this *MinStack) GetMin() int {
 	return this.n[len(this.m)-1]
 }
+
+//type MinStack struct {
+//	stack1 []int
+//	stack2 []int
+//}
+//
+//func Constructor() MinStack {
+//	m := MinStack{
+//		stack1: make([]int, 0),
+//		stack2: make([]int, 0),
+//	}
+//	return m
+//}
+//
+//func (this *MinStack) Push(val int) {
+//	this.stack1 = append(this.stack1, val)
+//	if len(this.stack2) == 0 || val <= this.stack2[len(this.stack2)-1] {
+//		this.stack2 = append(this.stack2, val)
+//	}
+//}
+//
+//func (this *MinStack) Pop() {
+//	val := this.stack1[len(this.stack1)-1]
+//	this.stack1 = this.stack1[:len(this.stack1)-1]
+//	if val == this.stack2[len(this.stack2)-1] {
+//		this.stack2 = this.stack2[:len(this.stack2)-1]
+//	}
+//}
+//
+//func (this *MinStack) Top() int {
+//	return this.stack1[len(this.stack1)-1]
+//}
+//
+//func (this *MinStack) GetMin() int {
+//	return this.stack2[len(this.stack2)-1]
+//}
