056bf58997b4f18dbd489cab357266d9d595769c
progzczhao <zcprog@foxmail.com>
2024-10-10 10:19:51 +0800

    0064.最小路径和

--- go-leetcode/classify_algorithm/dynamic_program/leetcode_0064_minimum-path-sum/leetcode_0064_minimum-path-sum.go
+++ go-leetcode/classify_algorithm/dynamic_program/leetcode_0064_minimum-path-sum/leetcode_0064_minimum-path-sum.go
@@ -7,8 +7,10 @@ package leetcode_0064_minimum_path_sum
 // 时间复杂度: O(m*n)
 // 空间复杂度: O(1)
 // 思路：
+//
 //	a.递归表达式：f(i,j)=min{f(i,j-1),f(i-1,j)}
 //	b.可以利用原数组进行数据存储，这样可以减少空间复杂度
+//
 // 条件：可以改变原数组
 func minPathSum(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
@@ -26,11 +28,13 @@ func minPathSum(grid [][]int) int {
 	return grid[m-1][n-1]
 }
 
-// minPathSum 动态规划
+// minPathSum_2 动态规划
 // 时间复杂度: O(m*n)
 // 空间复杂度: O(n)
 // 思路：
+//
 //	a.递归表达式：f(i,j)=min{f(i,j-1),f(i-1,j)}
+//
 // 条件：不可以改变原数组
 func minPathSum_2(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
@@ -48,6 +52,43 @@ func minPathSum_2(grid [][]int) int {
 		}
 	}
 	return dp[n-1]
+}
+
+// minPathSum_3 动态规划
+// 时间复杂度: O(m*n)
+// 空间复杂度: O(m*n)
+// 思路：
+//
+//	a.递归表达式：f(i,j)=min{f(i,j-1),f(i-1,j)}
+//
+// 条件：不可以改变原数组
+func minPathSum_3(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+		if i == 0 {
+			dp[i][0] = grid[i][0]
+		} else {
+			dp[i][0] = dp[i-1][0] + grid[i][0]
+		}
+	}
+
+	for j := 0; j < n; j++ {
+		if j == 0 {
+			dp[0][j] = grid[0][j]
+		} else {
+			dp[0][j] = dp[0][j-1] + grid[0][j]
+		}
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j])
+		}
+	}
+	return dp[m-1][n-1]
 }
 
 func min(x, y int) int {
