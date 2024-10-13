87afddc3cc047d98f46d35a40b6052a056f2de88
progzczhao <zcprog@foxmail.com>
2024-10-13 13:40:36 +0800

    0347.前K个高频元素

--- go-leetcode/classify_algorithm/array/leetcode_0347_top-k-frequent-elements/leetcode_0347_top-k-frequent-elements.go
+++ go-leetcode/classify_algorithm/array/leetcode_0347_top-k-frequent-elements/leetcode_0347_top-k-frequent-elements.go
@@ -1,8 +1,40 @@
 package leetcode_0347_top_k_frequent_elements
 
+import "sort"
+
 // 0347.前 K 个高频元素
 // https://leetcode.cn/problems/top-k-frequent-elements/
 
 func topKFrequent(nums []int, k int) []int {
+	type item struct {
+		num int
+		cnt int
+	}
+	n := len(nums)
+	m := make(map[int]*item)
+	for i := 0; i < n; i++ {
+		if v, ok := m[nums[i]]; ok {
+			v.cnt++
+			m[nums[i]] = v
+		} else {
+			m[nums[i]] = &item{nums[i], 1}
+		}
+	}
 
+	var (
+		ss  []*item
+		ans []int
+	)
+	for _, value := range m {
+		ss = append(ss, value)
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].cnt >= ss[j].cnt
+	})
+
+	for i := 0; i < len(ss) && i < k; i++ {
+		ans = append(ans, ss[i].num)
+	}
+	return ans
 }
