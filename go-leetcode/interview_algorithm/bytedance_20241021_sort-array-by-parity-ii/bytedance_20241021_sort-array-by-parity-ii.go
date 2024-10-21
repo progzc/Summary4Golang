9c6bd1010e256789ccec626fe6370b34962f6b04
progzczhao <zcprog@foxmail.com>
2024-10-21 21:19:48 +0800

    字节-按照奇偶排序数组

---
+++ go-leetcode/interview_algorithm/bytedance_20241021_sort-array-by-parity-ii/bytedance_20241021_sort-array-by-parity-ii.go
@@ -0,0 +1,28 @@
+package bytedance_20241021_sort_array_by_parity_ii
+
+// 字节一面：按照奇偶排序数组
+// 922. 按奇偶排序数组 II
+// https://leetcode.cn/problems/sort-array-by-parity-ii
+
+// sortArrayByParityII
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func sortArrayByParityII(nums []int) []int {
+	n := len(nums)
+	if n%2 == 1 || n <= 1 {
+		return nums
+	}
+	p1, p2 := 0, 1
+	for p1 < n && p2 < n {
+		for p1 < n && nums[p1]%2 == 0 {
+			p1 += 2
+		}
+		for p2 < n && nums[p2]%2 == 1 {
+			p2 += 2
+		}
+		if p1 < n && p2 < n {
+			nums[p1], nums[p2] = nums[p2], nums[p1]
+		}
+	}
+	return nums
+}
