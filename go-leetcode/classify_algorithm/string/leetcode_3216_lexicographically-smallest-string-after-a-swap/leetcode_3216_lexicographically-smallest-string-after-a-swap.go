35a35ebe182e60ea7ac6167b45595f844b315010
progzczhao <zcprog@foxmail.com>
2024-10-30 09:44:14 +0800

    3216. 交换后字典序最小的字符串

---
+++ go-leetcode/classify_algorithm/string/leetcode_3216_lexicographically-smallest-string-after-a-swap/leetcode_3216_lexicographically-smallest-string-after-a-swap.go
@@ -0,0 +1,37 @@
+package leetcode_3216_lexicographically_smallest_string_after_a_swap
+
+// 3216. 交换后字典序最小的字符串
+// https://leetcode.cn/problems/lexicographically-smallest-string-after-a-swap
+// 注意：最多交换一次
+
+// getSmallestString 顺序遍历
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
+func getSmallestString(s string) string {
+	ans := s
+	for i := 0; i < len(s)-1; i++ {
+		b := []byte(s)
+		x, y := int(s[i]), int(s[i+1])
+		if (x%2 == 0 && y%2 == 0) || (x%2 == 1 && y%2 == 1) {
+			b[i], b[i+1] = b[i+1], b[i]
+			if string(b) < ans {
+				ans = string(b)
+			}
+		}
+	}
+	return ans
+}
+
+// getSmallestString_2 顺序遍历
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+func getSmallestString_2(s string) string {
+	r := []rune(s)
+	for i := 0; i+1 < len(r); i++ {
+		if r[i] > r[i+1] && r[i]%2 == r[i+1]%2 {
+			r[i], r[i+1] = r[i+1], r[i]
+			break
+		}
+	}
+	return string(r)
+}
