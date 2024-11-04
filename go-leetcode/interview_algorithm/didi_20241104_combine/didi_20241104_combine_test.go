822ce8bcf695f581411566277f4b944bb9b6b3c8
progzczhao <zcprog@foxmail.com>
2024-11-04 16:04:51 +0800

    排列组合

---
+++ go-leetcode/interview_algorithm/didi_20241104_combine/didi_20241104_combine_test.go
@@ -0,0 +1,40 @@
+package didi_20241104_combine
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 滴滴（一面）
+// 排列组合
+
+func TestCombine(t *testing.T) {
+	nums := []int{1, 2, 3, 4}
+	fmt.Println(combine(nums)) // [[1 2 3 4] [1 2 4 3] [1 3 2 4] [1 3 4 2] [1 4 2 3] [1 4 3 2] [2 1 3 4] [2 1 4 3] [2 3 1 4] [2 3 4 1] [2 4 1 3] [2 4 3 1] [3 1 2 4] [3 1 4 2] [3 2 1 4] [3 2 4 1] [3 4 1 2] [3 4 2 1] [4 1 2 3] [4 1 3 2] [4 2 1 3] [4 2 3 1] [4 3 1 2] [4 3 2 1]]
+}
+
+func combine(nums []int) [][]int {
+	var (
+		ans [][]int
+		dfs func(idx int, output []int)
+	)
+	choose := make([]bool, len(nums))
+	dfs = func(idx int, output []int) {
+		if idx == len(nums) {
+			ans = append(ans, append([]int{}, output...))
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if choose[i] {
+				continue
+			}
+			choose[i] = true
+			output = append(output, nums[i])
+			dfs(idx+1, output)
+			output = output[:len(output)-1]
+			choose[i] = false
+		}
+	}
+	dfs(0, nil)
+	return ans
+}
