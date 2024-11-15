0d98306a2a0ab6bcee94d82da184a48a63c6b7a7
progzczhao <zcprog@foxmail.com>
2024-11-15 10:07:16 +0800

    在重复的升序数组中找到第一个不小于目标数的序号

---
+++ go-leetcode/interview_algorithm/bytedance_20241114_find-in-sorted-array/bytedance_20241114_find-in-sorted-array_test.go
@@ -0,0 +1,33 @@
+package bytedance_20241114_find_in_sorted_array
+
+import (
+	"fmt"
+	"testing"
+)
+
+//字节Es平台工程师（一面）
+//在重复的升序数组中找到第一个不小于目标数的序号
+
+func TestFind(t *testing.T) {
+	nums := []int{2, 2, 2, 2, 2, 2}
+	target := 3
+	fmt.Println(find(nums, target))
+}
+
+// find
+func find(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)>>2
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			if mid == 0 || nums[mid-1] < target {
+				return mid
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return -1
+}
