0f1e1ec66fbf376a979a8aa56c0ee21bce38d994
progzczhao <zcprog@foxmail.com>
2024-11-25 23:09:40 +0800

    小米-最大连续子矩阵的和

--- go-leetcode/interview_algorithm/xiaomi_20241122_max-submatrix-lcci/xiaomi_20241122_max-submatrix-lcci_test.go
+++ go-leetcode/interview_algorithm/xiaomi_20241122_max-submatrix-lcci/xiaomi_20241122_max-submatrix-lcci_test.go
@@ -2,19 +2,32 @@ package xiaomi_20241122_max_submatrix_lcci
 
 import (
 	"fmt"
+	"math"
 	"testing"
 )
 
-// 小米二面
+// 小米二面（端到端数据闭环工程师）
+
 // 算法题1: 最大连续子数组的和（mid）
 // 0053.最大子数组和
 // https://leetcode.cn/problems/maximum-subarray
 
-// 算法题2: 最大连续子矩阵的和（hard）
+// 算法题2: 最大连续子矩阵的和（hard）🌟
+// 面试题 17.24. 最大子矩阵
+// https://leetcode.cn/problems/max-submatrix-lcci
 
 func TestMaxSubArraySum(t *testing.T) {
 	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	fmt.Println(maxSubArraySum(nums)) // 6
+}
+
+func TestMaxSubMatrixSum(t *testing.T) {
+	matrix := [][]int{
+		{9, -8, 1, 3, -2},
+		{-3, 7, 6, -2, 4},
+		{6, -4, -4, 8, -7},
+	}
+	fmt.Println(maxSubMatrixSum(matrix)) // 19
 }
 
 // maxSubArraySum 最大连续子数组的和
@@ -37,6 +50,35 @@ func maxSubArraySum(nums []int) int {
 		}
 		if dp[i] > ans {
 			ans = dp[i]
+		}
+	}
+	return ans
+}
+
+// maxSubMatrixSum 最大连续子矩阵的和
+// 思路：动态规划+前缀和
+func maxSubMatrixSum(matrix [][]int) int {
+	m := len(matrix)
+	if m == 0 {
+		return 0
+	}
+	n := len(matrix[0])
+	var ans int
+	for beginLine := 0; beginLine < m; beginLine++ {
+		sum := make([]int, n)
+		for i := beginLine; i < m; i++ {
+			dp := math.MinInt32
+			for j := 0; j < n; j++ {
+				sum[j] += matrix[i][j]
+				if dp > 0 {
+					dp += sum[j]
+				} else {
+					dp = sum[j]
+				}
+				if dp > ans {
+					ans = dp
+				}
+			}
 		}
 	}
 	return ans
